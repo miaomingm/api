2385ee6049e839e477ffcbdceace1e60c6edcf7e
Your Name <you@example.com>
2020-06-19 15:37:31 +0800

    测试

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function hello(){
+        echo __METHOD__;echo '</br>';
+        echo date('Y-m-d H:i:s');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+Route::get('/test/hello','TestController@hello');
+
+//商品
+Route::get('/goods/detail','Goods\GoodsController@detail');//商品详情
